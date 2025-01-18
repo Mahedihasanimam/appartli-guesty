050d7f198a16a2c3dd2c41a34523aba77b2e4e11
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-01-18 11:06:31 +0600

    base api updated

--- src/redux/api/ApiSlice.js
+++ src/redux/api/ApiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://164.90.163.42:4000' ,
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://164.92.162.65:4000' ,
     prepareHeaders: (headers, { getState }) => {
       const state = getState();
       // console.log('Current state:', state); // Log entire state for debugging
